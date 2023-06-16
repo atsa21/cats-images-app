82b9adc21821ace62d4a2d4d2eb7f513694e9297
atsa21 <ts21ann@gmail.com>
2023-06-16 18:20:30 +0300

    firebase deploy fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { SharedModule } from './shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MainComponent } from './components/main/main.component';
 import { CatsCardComponent } from './components/main/cats-card/cats-card.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/main/header/header.component';
-import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from './shared.module';
 import { FiltersComponent } from './components/main/filters/filters.component';
 import { BreedsFiltersComponent } from './components/main/filters/breeds-filters/breeds-filters.component';
 import { LimitFiltersComponent } from './components/main/filters/limit-filters/limit-filters.component';
@@ -28,7 +31,8 @@ import { LimitFiltersComponent } from './components/main/filters/limit-filters/l
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
