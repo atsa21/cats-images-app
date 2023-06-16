004986a22dce9889af17b3e41a6c5fb03cb9a6fd
atsa21 <ts21ann@gmail.com>
2023-06-16 10:21:58 +0300

    add firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDqCUWSqcX0jwkKxTnvv5lpCIBcdHFVtgc",
+        authDomain: "cats-img-app.firebaseapp.com",
+        projectId: "cats-img-app",
+        storageBucket: "cats-img-app.appspot.com",
+        messagingSenderId: "321462162735",
+        appId: "1:321462162735:web:aee34f107368a1fad2fdda"
+    }
+};
